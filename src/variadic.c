02908330d7bc9587c1b69f6997a0b3fa3760821a
Simone <simonearcari@outlook.it>
2024-10-31 16:29:16 +0100

    versione con la define LOG_FILE

--- src/variadic.c
+++ src/variadic.c
@@ -1,32 +1,29 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-void log_message(FILE *file, const char *format, ...) {
+void log_message(const char *format, ...) {
     va_list args;
     va_start(args, format);
 
-    // Se il file è NULL, usa stdout come destinazione
-    FILE *output = file ? file : stdout;
 
-    // Stampa il messaggio utilizzando vfprintf, che accetta una lista di argomenti variabili
-    vfprintf(output, format, args);
+#ifdef LOG_FILE
+    FILE *logfile = fopen("log.txt", "a");
+    if (logfile) {
+        vfprintf(logfile, format, args);     
+        fclose(logfile);
+    } else {
+        perror("Errore nell'apertura del file di log");
+    }
+#else
+    vfprintf(stdout, format, args);
+#endif
 
     // Termina la lista di argomenti
     va_end(args);
 }
 
 int main() {
-    // Log su stdout
-    log_message(NULL, "Questo è un log su stdout: %d, %s\n", 42, "ciao");
-
-    // Log su un file specifico
-    FILE *logfile = fopen("log.txt", "a");
-    if (logfile) {
-        log_message(logfile, "Questo è un log su file: %f\n", 3.14);
-        fclose(logfile);
-    } else {
-        perror("Errore nell'apertura del file di log");
-    }
+    log_message("Questo è un log: %d, %s\n", 42, "ciao");
 
     return 0;
 }
